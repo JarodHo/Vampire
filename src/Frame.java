4c9454a5eccdf0457120aecb2dfad5424252b387
1935703 <1935703@01-19820.pusd.dom>
2022-03-29 08:50:40 -0700

    frame

--- src/Frame.java
+++ src/Frame.java
@@ -1,9 +1,78 @@
+public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
+	//creating objects such as characters, background, music/sound effects...
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		
+	public static void main(String[] arg) {
+		Frame f = new Frame();
 
-public class Frame {
-
-	public static void main(String[] args) {
+	}
+	public Frame() {
+		JFrame f = new JFrame("Vampire Survivors");
+		f.setSize(new Dimension(900, 900));
+		f.setBackground(Color.blue);
+		f.add(this);
+		f.setResizable(false);
+		f.setLayout(new GridLayout(1,2));
+		f.addMouseListener(this);
+		f.addKeyListener(this);
+		Timer t = new Timer(16, this);
+		t.start();
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+	}
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		//hi
+		
+		}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+	
+	
+	
+	}
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println(arg0.getKeyCode());
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
